ff93afc97a3be3fd55beab9327a844a314ffb575
hayoc <hayo.ce@gmail.com>
2017-02-03 23:23:33 +0100

    FEAT: daily keywords implementation

---
+++ projectxc6t73/semantics/article_search.py
@@ -0,0 +1,44 @@
+from nltk.corpus import wordnet as wn
+import logging
+import requests
+
+
+def new_york_times_request(date, page):
+    url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+    params = {
+        'api-key': '146f93e3db7345cc885b14999e9833d8',
+        'begin_date': date,
+        'end_date': date,
+        'fl': 'keywords',
+        'page': page
+    }
+    logging.info("Calling NYT API w/ params: %s" % str(params))
+    return requests.get(url, params=params)
+
+
+def daily_keywords(date):
+    """
+        Get 5 keywords for a particular day based on articles from New York Times
+    Args:
+        date: Day we should find keywords for (format:YYYYMMDD)
+    Returns:
+        list of keywords (ideally 5)
+    """
+    keywords = []
+    calls = 0
+    while len(keywords) < 5:
+        data = new_york_times_request(date, str(calls)).json()
+        calls += 1
+
+        for result in data['response']['docs']:
+            for keyword in result['keywords']:
+                if keyword['name'] == 'subject':
+                    value = keyword['value']
+                    word = value.split()[0]
+                    if wn.synsets(word):
+                        keywords.append(word)
+                    if len(keywords) >= 5:
+                        return keywords
+        # Stop if we've called API more than 10 times and still haven't found enough keywords
+        if calls > 10:
+            return keywords
